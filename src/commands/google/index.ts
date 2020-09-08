15ae73112af2144791290056ab0087d6e6b5ac75
VincentJonathan <vinjongm@gmail.com>
2020-09-08 17:12:54 +0700

    😡

--- src/commands/google/index.ts
+++ src/commands/google/index.ts
@@ -5,7 +5,6 @@ export = {
 	name: "google",
 	description: "GOOGLE IT!!!",
 	async execute(message: Message, args: string[]): Promise<string> {
-		console.log(123);
 		const url = `https://google.com/search?q=${encodeURIComponent(args.join(" "))}`;
 		await message.channel.send(url);
 		return url;
