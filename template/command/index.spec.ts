2e0de1192fbdf47014a6aad7e49c90d15f5cc28a
VincentJonathan <vinjongm@gmail.com>
2020-09-09 19:57:20 +0700

    Add ts-mockito for mocking object

--- template/command/index.spec.ts
+++ template/command/index.spec.ts
@@ -1,14 +1,21 @@
 import cmd from "./";
+import { mock, when, instance } from "ts-mockito";
+
+import { Message } from "discord.js";
 
 
 test("Test execute function", () => {
 	// Mocking Discord message object
-	const message = {
-		cleanContent: "Example"
-	};
-    
+	const mockedMessage:Message = mock(Message);
+	// Stubbing getter / read only attribute
+	when(mockedMessage.cleanContent).thenReturn("String clean");
+
+	const message = instance(mockedMessage);
+
 	// Mocking args
 	const args = ["args1", "args2"];
+	// or
+	// const args = "args1 args2".trim().split(/ +/);
 
-	cmd.execute(message as any, args);
+	cmd.execute(message, args);
 });
