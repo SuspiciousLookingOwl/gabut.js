63dd840e0f2a4bd9829464da663cfec20aa80692
VincentJonathan <vinjongm@gmail.com>
2020-09-10 13:59:47 +0700

    Simplify command template

--- template/command/index.spec.ts
+++ template/command/index.spec.ts
@@ -1,21 +1,6 @@
-import cmd from "./";
-import { mock, when, instance } from "ts-mockito";
-
-import { Message } from "discord.js";
+// import yourFunction from "./yourFunction";
 
 
 test("Test execute function", () => {
-	// Mocking Discord message object
-	const mockedMessage:Message = mock(Message);
-	// Stubbing getter / read only attribute
-	when(mockedMessage.cleanContent).thenReturn("String clean");
-
-	const message = instance(mockedMessage);
-
-	// Mocking args
-	const args = ["args1", "args2"];
-	// or
-	// const args = "args1 args2".trim().split(/ +/);
-
-	cmd.execute(message, args);
+	// except(yourFunction()).toBe("something");
 });
--- template/command/index.ts
+++ template/command/index.ts
@@ -1,20 +1,10 @@
 import { Command } from "discord.js";
-// Add required dependencies here
-// import Example from "some-module";
-// import OtherExample from "some-other-module";
 
 const command: Command = {
 	name: "[Your Command Name]",
 	description: "[Your Command Description]",
 	async execute(message, args) {
 		// Put your code here
-
-		// const firstArg = args.shift();
-		// const secondArg = args.shift();
-		
-		// await Example.test(firstArg);
-		// await OtherExample.test(secondArg);
-		// await message.channel.send("Something");
 	},
 };
 
