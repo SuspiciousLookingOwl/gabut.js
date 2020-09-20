fc119e861ff65fbe852d2c5299d962a2111bc96e
VincentJonathan <vinjongm@gmail.com>
2020-09-21 05:00:00 +0700

    Add support for HTML code from message

--- src/commands/html/index.ts
+++ src/commands/html/index.ts
@@ -10,18 +10,25 @@ export = {
 	description: "Send image preview from HTML",
 	async execute(message: Message, args: string[]): Promise<void> {
 		const attachments = message.attachments.array();
-		if (attachments.length === 0 || !attachments[0].url.endsWith(".html")) return;
-
+		let html = "";
+		if (attachments.length === 0 || !attachments[0].url.endsWith(".html")) {
+			const script = message.cleanContent.replace(`${process.env.PREFIX}${this.name}`  as string, "").replace(/```/g, "").trim();
+			const language = script.split(/\r?\n/)[0];
+			if (language.toLowerCase() !== "html") return;
+			html = script.slice(language.length);
+		} else {
+			const file = await axios.get(attachments[0].url);
+			html = file.data;
+		}
+		
 		const writeFile = promisify(fs.writeFile);
 		const unlink = promisify(fs.unlink);
-
 		const width = parseInt(args.shift() || "") || 1920;
 		const height = parseInt(args.shift() || "") || 1080;
 		const random = Math.random().toString(36).substring(7);
-		const file = await axios.get(attachments[0].url);
 
 		try {
-			await writeFile(`${__dirname}/${random}.html`, file.data);
+			await writeFile(`${__dirname}/${random}.html`, html);
 	
 			const browser = await puppeteer.launch({ headless: true });
 			const page = await browser.newPage();
