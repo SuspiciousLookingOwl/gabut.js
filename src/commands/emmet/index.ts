d1873909c3c7d035110766379d8f60c5e0eba6e0
VincentJonathan <vinjongm@gmail.com>
2020-12-02 21:52:06 +0700

    Add emmet command

---
+++ src/commands/emmet/index.ts
@@ -0,0 +1,18 @@
+import { Command } from "discord.js";
+import expand from "emmet";
+
+const command: Command = {
+	name: "emmet",
+	description: "Generate HTML from emmet",
+	async execute(message, args) {
+		const emmet = args.join(" ");
+		if (!emmet) return;
+		try {
+			await message.channel.send(`\`\`\`html\r\n${expand(emmet)}\`\`\``);
+		} catch (err) {
+			throw new Error("Invalid Emmet");
+		}
+	},
+};
+
+export = command;
