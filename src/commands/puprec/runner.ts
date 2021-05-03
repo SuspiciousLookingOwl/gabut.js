f3f5a94abee0409841df8c82e0620a2bcb3c6654
VincentJonathan <vinjongm@gmail.com>
2021-05-03 19:28:49 +0700

    remove logging

--- src/commands/puprec/runner.ts
+++ src/commands/puprec/runner.ts
@@ -3,7 +3,6 @@ import { Command } from "./commandParser";
 
 export default async (page: Page, commands: Command[]): Promise<void> => {
 	for (const { name, args } of commands) {
-		console.log(name, args);
 		// eslint-disable-next-line @typescript-eslint/ban-ts-comment
 		// @ts-ignore
 		await page[name](...args);
