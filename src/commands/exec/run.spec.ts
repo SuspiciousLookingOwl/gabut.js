491cf49a8842660ec485fc362495d7ead9f2bb7b
VincentJonathan <vinjongm@gmail.com>
2021-05-15 16:57:10 +0700

    add test for jdoodle

--- src/commands/exec/run.spec.ts
+++ src/commands/exec/run.spec.ts
@@ -1,6 +1,6 @@
 import { getLanguage, run } from "./run";
 import "dotenv/config";
-import { DenoTownResponse } from "./interfaces";
+import { DenoTownResponse, JDoodleResponse } from "./interfaces";
 
 test("Test run invalid language", async () => {
 	expect(run("wilson", "console.log(1);")).rejects.toThrow();
@@ -14,11 +14,10 @@ test("Test get language", () => {
 	expect(language.versionIndex).toBe(3);
 });
 
-// ! Temporarily disabling this test because it requires jDoodle credit
-// test("Test run javascript code", async () => {
-// 	const response = await run("js", "console.log(1);");
-// 	expect(response.output.trim()).toBe("1");
-// });
+test("Test run javascript code", async () => {
+	const response = (await run("js", "console.log(1);")) as JDoodleResponse;
+	expect(response.output.trim()).toBe("1");
+});
 
 test("Test run typescript code", async () => {
 	const response = (await run("typescript", "console.log(1);")) as DenoTownResponse;
