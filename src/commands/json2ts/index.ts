78a3e71173b87ed6e81da95f5dfc1fe9a6051df0
VincentJonathan <vinjongm@gmail.com>
2021-05-18 19:00:10 +0700

    add json2ts command

---
+++ src/commands/json2ts/index.ts
@@ -0,0 +1,22 @@
+import { Command } from "discord.js";
+import json2ts from "json-to-ts";
+import extractCode from "../../common/extractCode";
+
+const command: Command = {
+	name: "json2ts",
+	description: "Convert JSON to TypeScript Interface",
+	async execute(message) {
+		const scripts = extractCode(message.cleanContent);
+
+		for (const script of scripts) {
+			try {
+				const ts = json2ts(JSON.parse(script.content));
+				message.channel.send("```ts\r\n" + ts.join("\r\n\r\n") + "\r\n```");
+			} catch (err) {
+				continue;
+			}
+		}
+	},
+};
+
+export = command;
