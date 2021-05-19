7acc0602ed82ac056555f870f673d055fa5ed051
VincentJonathan <vinjongm@gmail.com>
2021-05-19 13:51:46 +0700

    add color command

---
+++ src/commands/color/index.ts
@@ -0,0 +1,21 @@
+import { Command, MessageAttachment } from "discord.js";
+import Color from "color";
+import { createCanvas } from "canvas";
+
+const command: Command = {
+	name: "color",
+	description: "Show color from color code",
+	async execute(message, args) {
+		const color = Color(args.join(" "));
+
+		const canvas = createCanvas(200, 200);
+		const ctx = canvas.getContext("2d");
+		ctx.fillStyle = color.hex();
+		ctx.fillRect(0, 0, 2000, 200);
+
+		const attachment = new MessageAttachment(canvas.toBuffer(), color.hex() + ".png");
+		await message.channel.send("", attachment);
+	},
+};
+
+export = command;
